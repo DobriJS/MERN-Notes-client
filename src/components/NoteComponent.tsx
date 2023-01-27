533102cd74be1d9306668c27b2eb2ca4417ee1e9
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-01-27 18:23:06 +0200

    Use pre defined function for updated notes timestamps

--- src/components/NoteComponent.tsx
+++ src/components/NoteComponent.tsx
@@ -1,9 +1,17 @@
 import { Card } from 'react-bootstrap';
 import styles from '../styles/NoteComponent.module.css';
 import { NoteProps } from '../interfaces/NoteProps';
+import { formatDate } from '../utils/formatDate';
 
 const NoteComponent = ({ note, className }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = 'Updated: ' + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = 'Created: ' + formatDate(createdAt);
+  }
 
   return (
     <Card className={`${styles.noteCard} ${className}`}>
@@ -11,6 +19,7 @@ const NoteComponent = ({ note, className }: NoteProps) => {
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
+      <Card.Footer className='text-muted'>{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
