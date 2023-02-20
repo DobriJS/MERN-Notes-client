fde7f47e0fc833284efaf394be5dabb82b534b68
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-02-20 16:33:41 +0200

    Toggle nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavBarProps } from '../interfaces';
+import NavBarLoggedInView from './NavBarLoggedInView';
+import NavBarLoggedOutView from './NavBarLoggedOutView';
 
 const NavBar = ({
   loggedInUser,
@@ -11,6 +13,19 @@ const NavBar = ({
     <Navbar bg='primary' variant='dark' expand='lg' sticky='top'>
       <Container>
         <Navbar.Brand>NOTES</Navbar.Brand>
+        <Navbar.Toggle aria-controls='main-navbar' />
+        <Navbar.Collapse id='main-navbar'>
+          <Nav className='ms-auto'>
+            {loggedInUser ? (
+              <NavBarLoggedInView user={loggedInUser} onLogoutSuccessful={onLogoutSuccessful} />
+            ) : (
+              <NavBarLoggedOutView
+                onLoginClicked={onLoginClicked}
+                onSignUpClicked={onSignUpClicked}
+              />
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
